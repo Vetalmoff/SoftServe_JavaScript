c6eca009c094b7e7592eb38a6637830595086337
Vitalii <Vetalmoff@gmail.com>
2020-03-01 04:17:41 +0200

    second task

--- js/task1.js
+++ js/task1.js
@@ -284,12 +284,12 @@
 
 
 
-//                      task 15
+//                      вывести число с порядком n
 
 
 // let ost = 0
 // let k =1234
-// let n = 8
+// let n = 3
 //  function digitN(k, n) {
 //     for (let i = 1; i <= n; i++) {
 //         ost = k % 10
@@ -307,3 +307,136 @@
 // digitN(k, n, ost)
 
 
+
+
+
+
+
+//                     Замыкания
+
+
+// function createArrayIterator(array) {
+//      let i = 0
+//     return function () {
+//         return array[i++]
+//     }
+// }
+// let arr = [5, 3, 7]
+// let itr = createArrayIterator(arr)
+// console.log(itr())
+// console.log(itr())
+// console.log(itr())
+// console.log(itr())
+
+
+
+//               Рекурсия сумма
+
+// let num = +prompt('input k', 5)
+
+// function digitSum( k ) {
+//     return ( k == 1 ) ? k : k + digitSum( k - 1 )
+// } 
+// digitSum(num)
+
+// alert(digitSum(num))
+
+// function outer(closureParameter) {
+//     var closureVariable = 2;
+//     return function (a) {
+//         return a + closureParameter + " " + closureVariable;
+//     };
+// }
+// var inner = outer(1)(3);
+// //var res = inner();
+// console.log(inner); // "1 2"
+
+
+
+
+
+
+//Напишите   код   для   "безопасного"   вычисления 
+// периметра   и   площадипрямоугольника.Сторонами прямоугольника 
+// может быть только число (тип данных  Number,значения других
+// типов данных нельзя устанавливать), значение которого,
+//строгобольше 0.
+
+// function safePerimetrCalculation() {
+//     let a = 0
+//     let b = 0
+//     let arr = []
+//     arr[0] = function(a_Side, b_Side) {
+//         if (typeof a_Side === 'number' && a_Side > 0  &&
+//             typeof b_Side === 'number' && b_Side > 0 ) {
+//             a = a_Side
+//             b = b_Side    
+//         }
+//     }    
+//     arr[1] = () => (2 * a + 2 * b)
+    
+//     arr[2] = () => (a * b)
+    
+//     return arr
+// }
+// let perim = safePerimetrCalculation()
+// perim[0](45, 3))
+// console.log(perim[1]())
+// console.log(perim[2]())
+
+
+
+
+//                      факториал
+
+// let fac = +prompt('input n', 10)
+// function factorial(n) {
+//     if ( n === 1 ) {
+//         return 1
+//     }
+//     else {
+//         return n * factorial(n - 1) 
+//     }
+// }
+// let a = factorial(fac)
+// alert(a)
+
+
+
+ 
+//                    переопределение
+
+
+// function format(data, type) {
+//     if (type === 'number') {
+//         format = function() {
+//             return +data
+//         }
+//     }
+//         else if (type === 'string') {
+//         format = function() {
+//             return String(data)
+//         }
+//     }
+//     else if (type === 'boolean') {
+//         format = function() {
+//             return Boolean(data)
+//         }
+//     }
+//     else {
+//         return
+//     }
+// }
+// let originFormat = format
+
+// format('1', 'number')
+// console.log(format())  // 1
+// console.log(typeof format())  // 'number'
+
+// originFormat('Hello', 'boolean')
+// console.log(format())  // true
+// console.log(typeof format())  // 'boolean'
+
+// originFormat(true, 'string')
+// console.log(format())  // 'true'
+// console.log(typeof format())  // 'string'
